865fa47990bff5622d40aeed5558f61b4c7eed46
akhildevj <akhildevj@qburst.com>
2022-03-13 13:48:09 +0530

    Implemented Favorite screen Pop Menu

--- lib/screens/products_overview.dart
+++ lib/screens/products_overview.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FILTER { favorites, all }
+
+class ProductsOverviewScreen extends StatefulWidget {
   const ProductsOverviewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showFavorites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Akz Shop')),
-      body: const ProductsGrid(),
+      appBar: AppBar(
+        title: const Text('Akz Shop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FILTER value) => {
+              setState(() {
+                if (value == FILTER.favorites) {
+                  _showFavorites = true;
+                } else {
+                  _showFavorites = false;
+                }
+              })
+            },
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                  child: Text('Favorites'), value: FILTER.favorites),
+              const PopupMenuItem(child: Text('Show All'), value: FILTER.all)
+            ],
+            icon: const Icon(Icons.more_vert),
+          )
+        ],
+      ),
+      body: ProductsGrid(showFavorites: _showFavorites),
     );
   }
 }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -4,11 +4,14 @@ import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/widgets/product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
-  const ProductsGrid({Key? key}) : super(key: key);
+  final bool showFavorites;
+
+  const ProductsGrid({Key? key, required this.showFavorites}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final products = Provider.of<Products>(context).items;
+    final provider = Provider.of<Products>(context);
+    final products = showFavorites ? provider.favoriteItems : provider.items;
 
     return GridView.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
